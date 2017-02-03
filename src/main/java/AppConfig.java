8255baeb91220abdd5b12fb3b8cc179d3bbb4b40
Alexandra Martinez <nahomibj@gmail.com>
2017-02-02 23:09:07 -0600

    Added configuration to read app.properties using annotations

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,10 +1,19 @@
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @ComponentScan({"com.martinez"})
+@PropertySource("app.properties")
 public class AppConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer getPropertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 
     /*
     @Bean(name = "customerService")
--- src/main/java/com/martinez/repository/HibernateCustomerRepositoryImpl.java
+++ src/main/java/com/martinez/repository/HibernateCustomerRepositoryImpl.java
@@ -5,13 +5,19 @@ import com.martinez.model.Customer;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 @Repository("customerRepository")
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 
+    @Value("${dbUsername}")
+    private String dbUsername;
+
     @Override
     public List<Customer> findAll() {
+        System.out.println(dbUsername);
+
         List<Customer> customers = new ArrayList<>();
 
         Customer customer = new Customer();
